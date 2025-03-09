9ca592776c67050237a22949de61f80d377f5ef4
Shashini Dias <117101873+anudz27@users.noreply.github.com>
2025-03-10 01:19:01 +0530

    login

--- src/test/java/LogInTest.java
+++ src/test/java/LogInTest.java
@@ -1,11 +1,14 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+
+import static org.testng.Assert.assertEquals;
 
 public class LogInTest {
    public String baseUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
@@ -20,20 +23,34 @@ public class LogInTest {
 
         driver.get(baseUrl);
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
     }
 
     @Test
     public void logITestValidCredential()
     {
-    driver.findElement(By.xpath("//input").sendKeys("Admin"));
+    driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("Admin");
 
+        driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("admin123");
+
+        driver.findElement(By.xpath("//button[@type='submit']")).submit();
+
+        String pageTitle = driver.getTitle();
+
+        Assert.assertNotEquals(pageTitle, "OrangeHRM");
     }
+
+//    public void logOut(){
+//    driver.findElement(By.xpath("//p[@class='oxd-userdropdown-name']")).click();
+//    driver.findElement(By.xpath("//p[normalize-space()='Logout']")).click();
+//
+//
+//    }
 
     @AfterTest
     public void tearDown() throws InterruptedException
     {
 
-
+    driver.quit();
     }
 }
